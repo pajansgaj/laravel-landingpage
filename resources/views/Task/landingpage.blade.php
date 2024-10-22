ab4081e090a36d70262a6988dbb8fee2dab73dc7
pajansgaj <byanhasyif@gmail.com>
2024-10-22 16:06:44 +0700

    add landingpage

---
+++ resources/views/Task/landingpage.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Landing Page</title>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+    <body>
+        <div class="header">
+        <div class="user-icon"></div>
+        <input type="text" placeholder="Search...">
+        <div class="settings-icon"></div>
+    </div>
+    <div class="container">
+        <div class="item">
+            <img src="image/monyet.jpg" alt="Monyet">
+            <p class="label">M O N Y E T</p>
+        </div>
+        <div class="item">
+            <img src="image/kera.jpg" alt="Kera">
+            <p class="label">K E R A</p>
+        </div>
+        <div class="item">
+            <img src="image/byan.jpg" alt="Byan">
+            <p class="label">B Y A N</p>
+        </div>
+    </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DepanController;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return "Hello World";
 });
 
 Route::get('/depan', [DepanController::class, 'index']);
+Route::get('/landingpage', [TaskController::class, 'index']);
